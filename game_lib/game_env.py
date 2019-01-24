3f3abf6dd711bd4a2740da391d75382f1eb467a9
Teshan Liyanage <teshanl@zone24x7.com>
2019-01-24 23:19:10 +0530

    initial commit

---
+++ game_lib/game_env.py
@@ -0,0 +1,172 @@
+import pygame
+import numpy as np
+
+from pygame.sprite import Sprite, Group
+
+
+class Mario(Sprite):
+    def __init__(self, image, resize_to, ducked_image, ducked_resize_to, X, Y, resolution, horizontal_step, jump_vel,
+                 gravity):
+        super().__init__()
+        self.image1 = pygame.transform.scale(pygame.image.load(image), resize_to)
+        self.image2 = pygame.transform.scale(pygame.image.load(ducked_image), ducked_resize_to)
+        self.image = self.image1
+        self.sizeX = pygame.Surface.get_width(self.image)
+        self.sizeY = pygame.Surface.get_height(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (X, Y)
+        self.init_Y = Y
+
+        self.sizeX1 = pygame.Surface.get_width(self.image1)
+        self.sizeY1 = pygame.Surface.get_height(self.image1)
+        self.rect1 = self.image1.get_rect()
+        self.sizeX2 = pygame.Surface.get_width(self.image2)
+        self.sizeY2 = pygame.Surface.get_height(self.image2)
+        self.rect2 = self.image2.get_rect()
+        self.ydiff = self.sizeY1 - self.sizeY2
+
+        self.resolution = resolution
+        self.horizontal_step = horizontal_step
+        self.jump_vel = jump_vel
+        self.gravity = gravity
+        self.jumping = False
+        self.is_ducked = False
+        self.t_jump = 0
+
+    def reset_pos(self):
+        x, _ = self.rect.topleft
+        self.image = self.image1
+        self.sizeX = self.sizeX1
+        self.sizeY = self.sizeY1
+        self.rect = self.rect1
+        self.rect.topleft = (x, self.init_Y)
+
+    def update(self, state):
+        if state == "duck" and not self.is_ducked:
+            x, y = self.rect.topleft
+            self.image = self.image2
+            self.sizeX = self.sizeX2
+            self.sizeY = self.sizeY2
+            self.rect = self.rect2
+            self.rect.topleft = (x, y + self.ydiff)
+            self.is_ducked = True
+        elif state == "fw":
+            self.rect.x = min(self.resolution[0] - self.sizeX, self.rect.x + self.horizontal_step)
+        elif state == "bw":
+            self.rect.x = max(0, self.rect.x - self.horizontal_step)
+        elif state == "jump" and not self.is_ducked:  # can't jump if ducked
+            self.jumping = True
+        elif state == "reset" and self.is_ducked:
+            x, y = self.rect.topleft
+            self.image = self.image1
+            self.sizeX = self.sizeX1
+            self.sizeY = self.sizeY1
+            self.rect = self.rect1
+            self.rect.topleft = (x, y - self.ydiff)
+            self.is_ducked = False
+
+        if self.jumping:
+            new_y = self.init_Y - (self.jump_vel * self.t_jump - 0.5 * self.gravity * self.t_jump ** 2)
+            if new_y > self.init_Y:
+                self.rect.y = self.init_Y
+                self.t_jump = 0
+                self.jumping = False
+            else:
+                self.rect.y = new_y
+                self.t_jump += 1
+
+
+class Mushroom(Sprite):
+    def __init__(self, image, resize_to, Y, resolution, released=False):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(image), resize_to)
+        self.sizeX = pygame.Surface.get_width(self.image)
+        self.sizeY = pygame.Surface.get_height(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (resolution[0], Y)
+
+        self.resolution = resolution
+        self.reset_pos()
+        self.released = released
+
+    def reset_pos(self):
+        self.velociy = np.random.randint(10, 20)
+        self.rect.x = self.resolution[0]
+        self.released = False
+
+    def update(self):
+        if not self.released:
+            return
+        new_x = self.rect.x - self.velociy
+        if new_x > 0:
+            self.rect.x = new_x
+        else:
+            self.reset_pos()
+
+
+class Fireball(Sprite):
+    def __init__(self, image, resize_to, Y, resolution, released=False):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(image), resize_to)
+        self.sizeX = pygame.Surface.get_width(self.image)
+        self.sizeY = pygame.Surface.get_height(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (resolution[0], Y)
+
+        self.resolution = resolution
+        self.reset_pos()
+        self.released = released
+
+    def reset_pos(self):
+        self.velociy = np.random.randint(20, 30)
+        self.rect.x = self.resolution[0]
+        self.released = False
+
+    def update(self):
+        if not self.released:
+            return
+        new_x = self.rect.x - self.velociy
+        if new_x > 0:
+            self.rect.x = new_x
+        else:
+            self.reset_pos()
+
+
+class ObstacleGroup(Group):
+    def __init__(self, release_count, release_dist):
+        super().__init__()
+        self.release_count = release_count
+        self.release_dist = release_dist
+
+    def update(self, *args):
+        sprites = self.sprites()
+        farthest = None
+        released = 0
+        for sprite in sprites:
+            if sprite.released:
+                released += 1
+                if not farthest or farthest.rect.x < sprite.rect.x:
+                    farthest = sprite
+
+        if released == 0:
+            sprites[np.random.randint(0, len(sprites))].released = True
+        elif farthest and farthest.rect.x < self.release_dist:
+            while released < self.release_count:
+                idx = np.random.randint(0, len(sprites))
+                if not sprites[idx].released:
+                    sprites[idx].released = True
+                    released += 1
+
+        super().update(*args)
+
+    def reset_pos(self):
+        for sprite in self.sprites():
+            sprite.reset_pos()
+
+class MarioGroup(Group):
+    def __init__(self):
+        super().__init__()
+
+    def reset_pos(self):
+        for sprite in self.sprites():
+            sprite.reset_pos()
---
+++ main.py
@@ -0,0 +1,125 @@
+from game_lib.game_env import Mario, Mushroom, Fireball, ObstacleGroup, MarioGroup
+import pygame
+
+resolution = (800, 400)
+
+caption = "Mario! :D"
+
+background_image = "images/background.png"
+mario_img_loc = "images/super-mario-png-paper-mario-icon-256.png"
+mushroom_img_loc = "images/cogumelo-do-mario-png-1.png"
+ducking_img_loc = "images/mario_crouh.png"
+fireball_img_loc = "images/fireball.png"
+
+jump_vel = 40
+gravity = 7
+frame_rate = 30
+# background_color = (100, 255, 100)
+bg = pygame.image.load(background_image)
+# player = Mario(123)
+
+RELEASED = True
+WAITING = False
+
+def get_key_event(game_over=False):
+    _state = None
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            _state = "quit"
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                _state = "jump"
+            elif event.key == pygame.K_DOWN:
+                _state = "duck"
+            elif event.key == pygame.K_LEFT:
+                _state = "bw"
+            elif event.key == pygame.K_RIGHT:
+                _state = "fw"
+        elif event.type == pygame.KEYUP:
+            if game_over:
+                if event.key == pygame.K_RETURN or event.key == pygame.K_SPACE:
+                    _state = "restart"
+                elif event.key == pygame.K_ESCAPE:
+                    _state = "quit"
+            else:
+                _state = "reset"
+    return _state
+
+def game_over_screen(screen):
+    _font = pygame.font.SysFont('Purisa', 50)
+    textsurface = _font.render('Game Over', False, (0, 0, 0))
+    screen.blit(textsurface, (screen_w // 2 - textsurface.get_width() // 2,
+                              screen_h // 2 - textsurface.get_height() // 2))
+    _font2 = pygame.font.SysFont('Purisa', 20)
+    textsurface2 = _font2.render('Press Enter to restart. ESC to quit', True, (0, 0, 0))
+    screen.blit(textsurface2, (screen_w // 2 - textsurface2.get_width() // 2,
+                               screen_h // 2 + 30))
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.font.init()
+    # initialize canvas
+    screen = pygame.display.set_mode(resolution)
+    clock = pygame.time.Clock()
+    pygame.display.set_caption(caption)
+
+    background = bg.convert()
+    screen.blit(background, (0, 0))
+
+    screen_w, screen_h = resolution
+    wlim, hlim = screen_w - 32, screen_h - 70
+
+    mario = Mario(image=mario_img_loc, resize_to=(32, 32), X=wlim * .1, Y=hlim,
+                  ducked_image=ducking_img_loc, ducked_resize_to=(28, 28),
+                  resolution=resolution, horizontal_step=10, gravity=gravity, jump_vel=jump_vel)
+
+    mushroom1 = Mushroom(image=mushroom_img_loc, resize_to=(32, 32), Y=hlim, resolution=resolution)
+    mushroom2 = Mushroom(image=mushroom_img_loc, resize_to=(32, 32), Y=hlim, resolution=resolution)
+    fireball1 = Fireball(image=fireball_img_loc, resize_to=(32, 32), Y=hlim - 28, resolution=resolution)
+    fireball2 = Fireball(image=fireball_img_loc, resize_to=(32, 32), Y=hlim - 28, resolution=resolution)
+
+    mario_group = MarioGroup()
+    mario_group.add(mario)
+    obstacle_group = ObstacleGroup(release_count=2, release_dist=wlim // 2)
+    obstacles = [mushroom1, mushroom2, fireball1, fireball2]
+    obstacle_group.add(obstacles)
+
+    game_over = False
+    quited = False
+    t0 = t1 = clock.get_time()
+
+    state = None
+    while True:
+        state = get_key_event(game_over)
+        if state == "quit":
+            break
+
+        if not game_over:
+            mario_group.update(state)
+            obstacle_group.update()
+
+            for obstacle in obstacles:
+                if mario.rect.colliderect(obstacle.rect):
+                    game_over = True
+                    break
+
+            screen.blit(background, (0, 0))
+
+            mario_group.draw(screen)
+            obstacle_group.draw(screen)
+
+        else:
+            if state == "restart":
+                mario_group.reset_pos()
+                obstacle_group.reset_pos()
+                game_over = False
+            else:
+                game_over_screen(screen)
+
+        pygame.display.update()
+        clock.tick(frame_rate)
+
+    print("Game Over")
+    # place("player", player.loc)
+    # play()
